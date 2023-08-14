4bdae18908884d9be47bf2534bdc92d9ffe6457e
marcosvaledev <111076997+marcosvaledev@users.noreply.github.com>
2023-08-14 12:57:48 -0300

    sem o html que eu tinha introduzido no inicio

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 function home(){
-    return <h1>Tatiane, eu amo você, se você me ama dá uma risadinha</h1>   
+
+    return <h1>Tatiane, eu amo você, se você me ama dá uma risadinha</h1>
+       
 }
 export default home;
