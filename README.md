7635481e75e2aac14a81886d085f1e4ed29d8b8d
Marcos Victor Sousa do Vale <111076997+marcosvaledev@users.noreply.github.com>
2023-08-24 13:33:00 -0300

    adicionando o prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Refazendo o site do Felipe Deschamps: https://tabnews.com.br para o aprendizado
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-function home(){
-
-    return <h1>Tatiane, eu amo você, se você me ama dá uma risadinhaaaaa</h1>
-       
+function home() {
+  return <h1>Tatiane, eu amo você, se você me ama dá uma risadinhaaaaa</h1>;
 }
 export default home;
+
+function arroz() {
+  console.log("sdasda");
+}
