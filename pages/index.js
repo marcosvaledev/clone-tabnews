8ccbd4e63ce479bb8b1440e06c7ff3a4fd4971d7
Marcos Victor Sousa do Vale <111076997+marcosvaledev@users.noreply.github.com>
2023-09-05 15:02:08 -0300

    adicionando as pastas

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,4 @@
 function home() {
-  return <h1>Tatiane, eu amo você, se você me ama dá uma risadinhaaaaa</h1>;
+  return <h1>Tatiane Eu amo você, se você me ama dá uma risadinhaaaaa</h1>;
 }
 export default home;
-
-function arroz() {
-  console.log("sdasda");
-}
